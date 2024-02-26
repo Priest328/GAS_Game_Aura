214bcf1417afa34c06ab15e3c6ec142f05baccf9
Martyniuk Maksym <krakenn606@gmail.com>
2024-02-26 14:41:06 +0200

    Add PlayerState and create Ability System Component, Attributes classes

--- Intermediate/Build/Win64/UnrealEditor/Inc/Aura/UHT/Aura.init.gen.cpp
+++ Intermediate/Build/Win64/UnrealEditor/Inc/Aura/UHT/Aura.init.gen.cpp
@@ -17,13 +17,13 @@ void EmptyLinkFunctionForGeneratedCodeAura_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x1FA945B1,
-				0xC1A7B395,
+				0x95361F25,
+				0x62BCD5DA,
 				METADATA_PARAMS(0, nullptr)
 			};
 			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_Aura.OuterSingleton, PackageParams);
 		}
 		return Z_Registration_Info_UPackage__Script_Aura.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_Aura(Z_Construct_UPackage__Script_Aura, TEXT("/Script/Aura"), Z_Registration_Info_UPackage__Script_Aura, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x1FA945B1, 0xC1A7B395));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_Aura(Z_Construct_UPackage__Script_Aura, TEXT("/Script/Aura"), Z_Registration_Info_UPackage__Script_Aura, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x95361F25, 0x62BCD5DA));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
--- Source/Aura/Aura.Build.cs
+++ Source/Aura/Aura.Build.cs
@@ -1,6 +1,7 @@
 // Copyright Maks Martyniuk Sample
 
 using UnrealBuildTool;
+using UnrealBuildTool.Rules;
 
 public class Aura : ModuleRules
 {
@@ -8,9 +9,9 @@ public class Aura : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "EnhancedInput",  });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] { "GameplayAbilities", "GameplayAbilitiesEditor", "GameplayTags","GameplayTasks" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
